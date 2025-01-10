6b9f6d1e2e491cc6dc4ece78f0e0d65b49dceda9
trong.kd <kienductrong@gmail.com>
2025-01-10 15:59:22 +0700

    Update server.js

--- server.js
+++ server.js
@@ -149,7 +149,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
